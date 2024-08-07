dd748920262a324d4828b6774cb9369b5f1de724
Orysse <abel.chartier@gmail.com>
2024-08-07 11:09:03 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ echo "Cloning GolluMOS Repository..."
 git clone https://github.com/SpirytusWeDrinkin/GolluMOS.git
 
 # Move and organize GolluMOS files
+echo "Creating ~/.GollumOS, the configuration will be saved in this directory, feel free to modify it afterward"
 cd || exit
 mkdir -p .GolluMOS
 cp -r GolluMOS/* .GolluMOS/
